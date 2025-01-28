0b239dcbf68a860123847141eefc1b2321b46844
Ghislain <ghislain.rouquette@outlook.fr>
2025-01-28 15:32:50 +0100

    us001 OK

---
+++ public/js/form.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const dateField = document.getElementById("date");
+  if (dateField) {
+    dateField.value = new Date().toISOString();
+  }
+});
